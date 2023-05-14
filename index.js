3ccd8dd81a87d0323cc4f63f2679fa2b9da63af9
joão <42720127+itsnotjohn@users.noreply.github.com>
2023-05-14 18:21:34 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+(async () => {
+    const config = require('./config.json');
+    const fs = require("fs");
+
+    const response = await fetch(`https://discord.com/api/guilds/${config.guildId}`, {
+        headers: {
+            'Authorization': config.token,
+            'Content-Type': 'application/json'
+        }
+    });
+
+    const res = await response.json();
+    const stickersInfo = res?.stickers;
+
+    for (let i = 0; i < stickersInfo.length; i++) {
+        const sticker = stickersInfo[i];
+        const stickerURL = `https://media.discordapp.net/stickers/${sticker?.id}.png?size=2048`;
+
+        const filePath = `${__dirname}/stickers/`;
+        const attachmentPath = `${filePath}/${sticker?.id}.png`;
+
+        if (!fs.existsSync(filePath)) fs.mkdirSync('stickers');
+
+        const response = await fetch(stickerURL);
+        const buffer = await response.arrayBuffer();
+
+        fs.createWriteStream(attachmentPath).write(Buffer.from(buffer));
+        console.log(`Figurinha baixada: ${sticker?.name} (${sticker?.id})`);
+    }
+})();
